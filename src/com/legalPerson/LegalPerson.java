03c3e21699add1ae61991bfbc9fc9b290ae8b7ca
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2023-09-04 19:37:21 -0300

    Adicionando classe utilitária LegalPerson
    LegalPerson é uma classe utilitária que fornece validação de CNPJ

---
+++ src/com/legalPerson/LegalPerson.java
@@ -0,0 +1,35 @@
+package com.legalPerson;
+
+public interface LegalPerson
+{
+    static public boolean cnpjIsValid(String cnpj)
+    {
+        if (cnpj.matches("(\\d)\\1{13}") || cnpj.strip().length() != 14)
+            return false;
+
+        int[] cnpjDigits = new int[14];
+        for (int i = 0; i < 14; i++)
+            cnpjDigits[i] = Character.getNumericValue(cnpj.charAt(i));
+
+        int[] weights = {5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2};
+    
+        int sum = 0;
+        for (int i = 0; i < 12; i++)
+            sum += cnpjDigits[i] * weights[i];
+
+        int rest = sum % 11;
+        int expectedDigit = (rest < 2) ? 0 : 11 - rest;
+        if (cnpjDigits[12] != expectedDigit)
+            return false;
+
+        weights = new int[]{6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2};
+
+        sum = 0;
+        for (int i = 0; i < 13; i++)
+            sum += cnpjDigits[i] * weights[i];
+
+        rest = sum % 11;
+        expectedDigit = (rest < 2) ? 0 : 11 - rest;
+        return cnpjDigits[13] == expectedDigit;
+    }
+}
