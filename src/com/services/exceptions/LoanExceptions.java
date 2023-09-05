bae0f8e2f9bf9a2d5c887a7191a7205f983dac95
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2023-09-04 21:54:02 -0300

    Criadas as classes de exceções para a futura classe Loan

--- src/com/services/exceptions/LoanExceptions.java
+++ src/com/services/exceptions/LoanExceptions.java
@@ -1,8 +1,10 @@
 package com.services.exceptions;
 
+import com.services.exceptions.ServiceExceptions.ServiceException;
+
 public class LoanExceptions
 {
-    public static abstract class LoanException extends Exception
+    public static abstract class LoanException extends ServiceException
     {
         public LoanException(String message)
         {
@@ -15,6 +17,26 @@ public class LoanExceptions
         static final String MESSAGE = "Valor de empréstimo inválido";
 
         public InvalidValueException()
+        {
+            super(MESSAGE);
+        }
+    }
+
+    public static class InvalidPortionsException extends LoanException
+    {
+        static final String MESSAGE = "Quantidade de parcelas inválida";
+
+        public InvalidPortionsException()
+        {
+            super(MESSAGE);
+        }
+    }
+
+    public static class InvalidEndDateException extends LoanException
+    {
+        static final String MESSAGE = "Data de término do empréstimo inválida";
+
+        public InvalidEndDateException()
         {
             super(MESSAGE);
         }
